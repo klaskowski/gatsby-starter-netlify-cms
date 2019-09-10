94ffcc5bca456cbfc4e84c2a53f52df43b86351b
Krystian Laskowski <krystian.laskowski.1@gmail.com>
2019-09-09 17:37:54 -0700

    Temporare solution to preview styling

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,4 +1,6 @@
 import CMS from 'netlify-cms-app'
 import ReportPreview from './preview-templates/ReportPreview'
 
+CMS.registerPreviewStyle("/css/layout.css");
+CMS.registerPreviewStyle("/css/3.css");
 CMS.registerPreviewTemplate('report', ReportPreview)
